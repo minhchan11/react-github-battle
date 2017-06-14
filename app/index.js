31ff906be77a9cea043c7df6f0c7d6dd800f7ba6
Minh Phuong <mphuong@kent.edu>
2017-06-14 15:23:29 -0700

    add components folder

--- app/index.js
+++ app/index.js
@@ -1,36 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var PropTypes = require('prop-types');
+var App = require('./components/App');
 
 require('./index.css');
 
-class Badge extends React.Component {
-  render() {
-    return (
-      <div>
-        <img
-          src={this.props.img}
-          alt="Avatar"
-          style={{width: 100, height:100}}
-          />
-          <h4>Name: {this.props.name} </h4>
-          <h5>Username: {this.props.username} </h5>
-      </div>
-    )
-  }
-}
-//type validation
-Badge.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-}
 
 
 ReactDOM.render(
-  <Badge
-    name="Tyler"
-    username="something"
-    img={{ 'image': 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'}} />,
+  <App/>,
   document.getElementById('app')
 );
--- exercise.js
+++ exercise.js
@@ -238,3 +238,35 @@ Users.propTypes = {
       friend: PropTypes.bool.isRequired,
     }))
   }
+
+//Prop types with css
+class Badge extends React.Component {
+  render() {
+    return (
+      <div>
+        <img
+          src={this.props.img}
+          alt="Avatar"
+          style={{width: 100, height:100}}
+          />
+          <h4>Name: {this.props.name} </h4>
+          <h5>Username: {this.props.username} </h5>
+      </div>
+    )
+  }
+}
+//type validation
+Badge.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+}
+
+
+ReactDOM.render(
+  <Badge
+    name="Tyler"
+    username="something"
+    img={{ 'image': 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'}} />,
+  document.getElementById('app')
+);
