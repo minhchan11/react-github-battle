9e924c26e34f0a634b925f77e3e8be8a1b16e854
Minh Phuong <mphuong@kent.edu>
2017-06-19 09:47:36 -0700

    add build commands

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports = {
+// NODE ENV to production
+// uglify the codes
+var config = {
   entry:'./app/index.js',
   output: {
     path: path.resolve(__dirname,'dist'),
@@ -20,4 +23,19 @@ module.exports = {
   plugins: [new HtmlWebpackPlugin({
     template: 'app/index.html'
   })]
+};
+
+if (process.env.NODE_ENV === 'production') {
+  //the above is for the commandline
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV) // this is to define the plugin
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
 }
+
+
+module.exports = config;
